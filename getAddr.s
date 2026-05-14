5cc57e0b75365ab4fa5d8a25ff2eed6fb66ea755
Greg Terteryan <100816343+GregTerteryan@users.noreply.github.com>
2026-05-13 19:33:11 -0700

    Finish(?) getAddr.s
    Perhaps finished? Needs testing.

--- getAddr.s
+++ getAddr.s
@@ -3,13 +3,16 @@ getAddr:
         //  X5: The address of (pointer to) the first value of the matirx.
         //  X6: The row of the element(0 indexed).
         //  X7: The column of the element(0 indexed).
-        //  X8: The stride of the matirx(how many elements to skip to get to the next row).
+        //  X8: The stride of the matrix(how many elements to skip to get to the next row).
 
         //   Output:
         //   X5: The address of (pointer to) the desired element of the matrix.
 
         //YOUR CODE STARTS HERE
 
-
+	// elements skipped = row*stride + col
+	UMUL X9, X6, X8 // row*stride
+	ADD X9, X9, X7 // row*stride + col
+	ADD X5, X5, X9 // skip X9 amount of elements
 
         //YOUR CODE ENDS HERE
